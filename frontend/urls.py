7f94c4c5703cbdd76789f16d30e3db49c41b19f2
MADS_STUDIO <mads.silelan@gmail.com>
2019-09-21 00:32:51 +0530

    update Pagination in gallery and achievements

--- frontend/urls.py
+++ frontend/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     path('achivements/',AchivementsView.as_view(), name='achivements'),
     path('about/', AboutView.as_view(), name='about'),
     path('history/', HistoryView.as_view(), name='history'),
-    path('career/', CareerView.as_view(), name='career'),    
+    path('career/', CareerView.as_view(), name='career'),   
+    path('event/', EventView.as_view(), name='event'),   
+    path('blog/', BlogView.as_view(), name='blog'),    
     path('contact/', ContactView.as_view(), name='contact'),
     path('success/', SuccessView.as_view(), name='success'),
     
